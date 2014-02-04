e11da9922eb41b463d325e6604d979caa93a0147
Danny Sepler <dannysepler@gmail.com>
2014-02-04 14:07:01 -0500

    drafted an insurance page

--- layout/insurance.php
+++ layout/insurance.php
@@ -2,5 +2,34 @@
 	$pageTitle = "| Insurance"; 
 	include 'header.php'; 
 ?>
+	<div class="row tour">
+		<div class="col-md-3">
+			<h2>Insurance Information</h2>
+		</div>
+		<div class="col-md-9">
+		</div>
+  	</div>
+
+  	<div class="row team">
+		<h2>We Accept</h2>
+		  <div class="col-lg-4"> <!-- Aetna -->
+			  <img img src="http://static.ehealthinsurance.com/ehealthinsurance/CarrierProfile/logos/AetnaPA.gif" alt="140x140">
+		  </div>
+		  <div class="col-lg-4"> <!-- IHCG -->
+			  <img img src="http://static.ehealthinsurance.com/ehealthinsurance/CarrierProfile/logos/IHCGroupLogo.gif" alt="140x140">
+		  </div>
+		  <div class="col-lg-4"> <!-- Humana -->
+			  <img img src="http://static.ehealthinsurance.com/ehealthinsurance/CarrierProfile/logos/Humana.gif" alt="140x140">
+		  </div>
+		  <div class="col-lg-4"> <!-- BlueCross -->
+			  <img img src="http://www.medicarebenefitsdirect.com/wp-content/uploads/2011/03/blue-cross-pic1.gif" alt="140x140">
+		  </div>
+		  <div class="col-lg-4"> <!-- Assurant -->
+			  <img img src="http://static.ehealthinsurance.com/ehealthinsurance/CarrierProfile/logos/Assurant.gif" alt="140x140">
+		  </div>
+		  <div class="col-lg-4"><!-- Kaiser Permanente -->
+			  <img img src="http://static.ehealthinsurance.com/ehealthinsurance/CarrierProfile/logos/KaiserPermanenteGA.gif" alt="140x140">
+		  </div>
+	  </div>
 
 <?php include 'footer.php'; ?>
