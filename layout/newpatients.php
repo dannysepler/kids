8964587e573d99dbe9c3da3e26df5b83c34c9619
Danny Sepler <dannysepler@gmail.com>
2014-02-09 10:57:56 -0500

    added col-md to the new patients page

--- layout/newpatients.php
+++ layout/newpatients.php
@@ -20,44 +20,72 @@
   	</div>
 
   	<div class="row team">
-  		<h2>Information for New Patients</h2>
-  		<p>
-  			Welcome to Kids Doc Pediatrics. 
-  			We are looking forward to 
-  			meeting you. Please fill out the 
-  			forms below and bring them to your 
-  			first visit.
-  		</p>
-  		<p>
-  			Also, be advised of our privacy
-  			practices by reading the
-  			<a href="#">Notice of Privacy Practices</a>
-  		</p>
+      <div class="col-md-3">
+        <!-- ENGLISH -->
+    		<h2>Information for New Patients</h2>
+    		<p>
+    			Welcome to Kids Doc Pediatrics. 
+    			We are looking forward to 
+    			meeting you. Please fill out the 
+    			forms below and bring them to your 
+    			first visit.
+    		</p>
+      </div>
+  		<div class="col-md-3">
+        <p>
+    			Also, be advised of our privacy
+    			practices by reading the
+    			<a href="#">Notice of Privacy Practices</a>
+    		</p>
+      </div>
+      <div class="col-md-3">
+    		<p><a href="#">Welcome</a></p>
+      </div>
+      <div class="col-md-3">
+    		<p><a href="#">Developmental</a></p>
+      </div>
+      <div class="col-md-3">
+    		<p><a href="#">Informed Consent</a></p>
+      </div>
+      <div class="col-md-3">
+    		<p><a href="#">HIPPA</a></p>
+      </div>
+      <div class="col-md-3">
+    		<p><a href="#">Records Release</a></p>
+      </div>
 
-  		<p><a href="#">Welcome</a></p>
-  		<p><a href="#">Developmental</a></p>
-  		<p><a href="#">Informed Consent</a></p>
-  		<p><a href="#">HIPPA</a></p>
-  		<p><a href="#">Records Release</a></p>
-
-  		<h2>Informacion para Pacientos Nuevos</h2>
-  		<p>
-  			Bienvenidos a Kids Doc Pediatrics,
-  			esperamos verlos pronto. Por favor
-  			llene las siguientes formas antes
-  			de su primera visita con el doctor.
-  		</p>
-  		<p>
-  			Recuerdo de leer como guardamos su 
-  			privacidad en nuestro
-  			<a href="#">Notice of Privacy Practices</a>
-  		</p>
-  		<p><a href="#">Welcome</a></p>
-  		<p><a href="#">Developmental</a></p>
-  		<p><a href="#">Informed Consent</a></p>
-  		<p><a href="#">HIPPA</a></p>
-  		<p><a href="#">Records Release</a></p>
-
+      <!-- SPANISH -->
+      <div class="col-md-3">
+        <h2>Informacion para Pacientos Nuevos</h2>
+        <p>
+          Bienvenidos a Kids Doc Pediatrics,
+          esperamos verlos pronto. Por favor
+          llene las siguientes formas antes
+          de su primera visita con el doctor.
+        </p>
+      </div>
+      <div class="col-md-3">
+        <p>
+          Recuerdo de leer como guardamos su 
+          privacidad en nuestro
+          <a href="#">Notice of Privacy Practices</a>
+        </p>
+      </div>
+      <div class="col-md-3">
+        <p><a href="#">Welcome</a></p>
+      </div>
+      <div class="col-md-3">
+        <p><a href="#">Developmental</a></p>
+      </div>
+      <div class="col-md-3">
+        <p><a href="#">Informed Consent</a></p>
+      </div>
+      <div class="col-md-3">
+        <p><a href="#">HIPPA</a></p>
+      </div>
+      <div class="col-md-3">
+        <p><a href="#">Records Release</a></p>
+      </div>
   	</div>
 
 <?php include 'footer.php'; ?>
