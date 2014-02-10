adf9a5442c2de3cc4f595823d5e4f2dc593965a5
David Flores <david@eatable.at>
2014-02-10 18:26:16 -0500

    Fixed new patients php and css files

--- layout/newpatients.php
+++ layout/newpatients.php
@@ -19,71 +19,28 @@
 		</div>
   	</div>
 
-  	<div class="row team">
-      <div class="col-md-3">
-        <!-- ENGLISH -->
-    		<h2>Information for New Patients</h2>
-    		<p>
-    			Welcome to Kids Doc Pediatrics. 
-    			We are looking forward to 
-    			meeting you. Please fill out the 
-    			forms below and bring them to your 
-    			first visit.
-    		</p>
-      </div>
-  		<div class="col-md-3">
-        <p>
-    			Also, be advised of our privacy
-    			practices by reading the
-    			<a href="#">Notice of Privacy Practices</a>
-    		</p>
-      </div>
-      <div class="col-md-3">
-    		<p><a href="#">Welcome</a></p>
-      </div>
-      <div class="col-md-3">
-    		<p><a href="#">Developmental</a></p>
-      </div>
-      <div class="col-md-3">
-    		<p><a href="#">Informed Consent</a></p>
-      </div>
-      <div class="col-md-3">
-    		<p><a href="#">HIPPA</a></p>
-      </div>
-      <div class="col-md-3">
-    		<p><a href="#">Records Release</a></p>
-      </div>
+  	<div class="row pt-background">
+		<div class="col-md-6">
+			<!-- ENGLISH -->
+			<h2>Information for New Patients</h2>
+			<p>Welcome to Kids Doc Pediatrics. We are looking forward to meeting you. Please fill out the forms below and bring them to your first visit.</p>
+			<p>Also, be advised of our privacy	practices by reading the <a href="#">Notice of Privacy Practices</a></p>
+			<p><a href="#">Welcome</a></p>
+			<p><a href="#">Developmental</a></p>
+			<p><a href="#">Informed Consent</a></p>
+			<p><a href="#">HIPPA</a></p>
+			<p><a href="#">Records Release</a></p>
+		</div>
 
       <!-- SPANISH -->
-      <div class="col-md-3">
+      <div class="col-md-6">
         <h2>Informacion para Pacientos Nuevos</h2>
-        <p>
-          Bienvenidos a Kids Doc Pediatrics,
-          esperamos verlos pronto. Por favor
-          llene las siguientes formas antes
-          de su primera visita con el doctor.
-        </p>
-      </div>
-      <div class="col-md-3">
-        <p>
-          Recuerdo de leer como guardamos su 
-          privacidad en nuestro
-          <a href="#">Notice of Privacy Practices</a>
-        </p>
-      </div>
-      <div class="col-md-3">
+        <p>Bienvenidos a Kids Doc Pediatrics, esperamos verlos pronto. Por favor llene las siguientes formas antes de su primera visita con el doctor.</p>
+        <p>Recuerdo de leer como guardamos su privacidad en nuestro<a href="#"> Notice of Privacy Practices</a>.</p>    
         <p><a href="#">Welcome</a></p>
-      </div>
-      <div class="col-md-3">
         <p><a href="#">Developmental</a></p>
-      </div>
-      <div class="col-md-3">
         <p><a href="#">Informed Consent</a></p>
-      </div>
-      <div class="col-md-3">
         <p><a href="#">HIPPA</a></p>
-      </div>
-      <div class="col-md-3">
         <p><a href="#">Records Release</a></p>
       </div>
   	</div>
