ffc952007d30025d75d48dff0b9cd5e311c08501
Danny Sepler <dannysepler@gmail.com>
2014-03-02 15:04:51 -0500

    styled staff descriptions and photos

--- layout/staff.php
+++ layout/staff.php
@@ -16,38 +16,38 @@
   	</div>
 
   	<div class="row team">
-  	  <div class="col-lg-4" id="dr_s">
+  	  <div class="col-lg-4 float_left" id="dr_s">
 		  <img class="img-circle" id="dr_img" img src="../dist/img/team/head-shots/dr.jpg" alt="140x140">
 		  <h2>Dr. Scaccabarrozzi<br><small>Paediatrician</small></h2>
-		  <p>Luis E. Scaccabarrozzi, M.D., MPH., is a pediatrician in Gainesville. Dr. Scaccabarrozzi graduated from the San Martin de Porres University – Faculty of Medicine in Lima, Peru.  In Peru, he served as the Director of the Municipal Health Center in the main port of Peru (Callao).  He was the coordinator for many immunizations programs and health care of children in the community.  He completed a Masters of Public Health at the University of Illinois.  Upon completion of his master’s degree he was involved in research at the University of Illinois, Erie Family Health Center, Centro San Bonifacio, and the Federal Bureau of Investigation.  In 2004, he completed his post graduate Pediatric Medical Residency at St. Louis Children’s Hospital in St. Louis, Missouri.  After graduating he settled in Gainesville in private practice until establishing Kids Doc Pediatrics in 2007. He participates professionally as a member of the University of Florida – Pediatrics After Hours as an Adjunct Assistant Professor.   He has privileges at Shands at AGH, Shands at University of Florida, and North Florida Regional Medical Center</p>
+		  <p class="p1left">Luis E. Scaccabarrozzi, M.D., MPH., is a pediatrician in Gainesville. Dr. Scaccabarrozzi graduated from the San Martin de Porres University – Faculty of Medicine in Lima, Peru.  In Peru, he served as the Director of the Municipal Health Center in the main port of Peru (Callao).  He was the coordinator for many immunizations programs and health care of children in the community.  He completed a Masters of Public Health at the University of Illinois.  Upon completion of his master’s degree he was involved in research at the University of Illinois, Erie Family Health Center, Centro San Bonifacio, and the Federal Bureau of Investigation.  In 2004, he completed his post graduate Pediatric Medical Residency at St. Louis Children’s Hospital in St. Louis, Missouri.  After graduating he settled in Gainesville in private practice until establishing Kids Doc Pediatrics in 2007. He participates professionally as a member of the University of Florida – Pediatrics After Hours as an Adjunct Assistant Professor.   He has privileges at Shands at AGH, Shands at University of Florida, and North Florida Regional Medical Center</p>
 		  <p>Dr. Scaccabarrozzi is a resident of Gainesville.  Outside of work, he enjoys spending time with his wife and 3 children (Gianella, Andrea, and Piero).  </p>
 	  </div><!-- /.col-lg-4 -->
 
-	  <div class="col-lg-4" id="mrs_s">
+	  <div class="col-lg-4  float_right" id="mrs_s">
 		  <img class="img-circle" id="mrs_s_img" src="../dist/img/team/head-shots/mrs.JPG" alt="140x140">
 		  <h2>Mrs. Scaccabarrozzi<br><small>Office Manager</small></h2>
 		  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 	  </div><!-- /.col-lg-4 -->
 
-	  <div class="col-lg-4" id="n1">
+	  <div class="col-lg-4 float_left" id="n1">
 		  <img class="img-circle" id="n1_img" src="../dist/img/team/head-shots/n1.jpg" alt="140x140">
 		  <h2>Nurse Name<br><small>Registered Nurse</small></h2>
-		  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+		  <p class="p1left">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 	  </div><!-- /.col-lg-4 -->
 
-	  <div class="col-lg-4" id="n2">
+	  <div class="col-lg-4  float_right" id="n2">
 		  <img class="img-circle" id="n2_img" src="../dist/img/team/head-shots/n2.jpg" alt="140x140">
 		  <h2>Nurse Name<br><small>Registered Nurse</small></h2>
 		  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 	  </div><!-- /.col-lg-4 -->	
 
-	  <div class="col-lg-4" id="n3">
+	  <div class="col-lg-4 float_left" id="n3">
 		  <img class="img-circle" id="n3_img" src="../dist/img/team/head-shots/n3.jpg" alt="140x140">
 		  <h2>Nurse Name<br><small>Registered Nurse</small></h2>
-		  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+		  <p class="p1left">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 	  </div><!-- /.col-lg-4 -->
 
-	  <div class="col-lg-4" id="n4">
+	  <div class="col-lg-4  float_right" id="n4">
 		  <img class="img-circle" id="n4_img" img src="../dist/img/team/head-shots/n4.jpg" alt="140x140">
 		  <h2>Nurse Name<br><small>Registered Nurse</small></h2>
 		  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
