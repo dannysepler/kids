680d2c85a6835ba2bdbf76f0299eab920757a2a3
Danny Sepler <dannysepler@gmail.com>
2014-03-02 19:19:10 -0500

    added parent resources banner

--- layout/parent.php
+++ layout/parent.php
@@ -1,24 +1,19 @@
 <?php 
 	$pageTitle 	= '| Parents';
-	$css 		    = "";
-  $js         = "none";
-  $else       = "";
+	$css 		= "parent";
+  	$js         = "none";
+  	$else       = "";
 
     /* for CSS to highlight header which page user is on */
-  $immu		="";
-  $new 		="";
-  $cont 	="";
-  $pare 	="class = \"active\"";
-  $staf		="";
+  	$immu		="";
+  	$new 		="";
+  	$cont 		="";
+  	$pare 		="class = \"active\"";
+  	$staf		="";
 
 	include 'header.php'; 
 ?>
-	<div class="row tour">
-		<div class="col-md-3">
-			<h2>Parent Resources</h2>
-		</div>
-		<div class="col-md-9">
-		</div>
+	<div class="row banner">
   	</div>
 
   	<div class="row team">
